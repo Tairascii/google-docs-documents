3fbad992a2d738d4214cf65042a4de3036d7b06b
Tair Sairanbekov <tairtopskiy@gmail.com>
2025-01-28 21:23:57 +0500

    chore(DOC-7): transfer json field into camelCase

--- internal/app/handler/models.go
+++ internal/app/handler/models.go
@@ -2,18 +2,18 @@ package handler
 
 type CreateDocumentPayload struct {
 	Title          string `json:"title,omitempty"`
-	InitialContent string `json:"initial_content,omitempty"`
+	InitialContent string `json:"initialContent,omitempty"`
 }
 
 type CreateDocumentResponse struct {
-	DocumentID string `json:"document_id"`
+	DocumentID string `json:"documentId"`
 }
 
 type Document struct {
 	Id             string `json:"id"`
 	Title          string `json:"title"`
 	OwnerId        string `json:"owner_id"`
-	InitialContent string `json:"initial_content"`
-	RoomId         string `json:"room_id"`
-	OrgId          string `json:"org_id"`
+	InitialContent string `json:"initialContent"`
+	RoomId         string `json:"roomId"`
+	OrgId          string `json:"orgId"`
 }
